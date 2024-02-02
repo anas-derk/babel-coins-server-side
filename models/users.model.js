d96a7a047e259ec500f695e777168ad7589fe053
anas derk <anas.derk2020@gmail.com>
2024-02-02 07:10:41 -0800

    Add Address Value To Get All Accounts Res

--- models/users.model.js
+++ models/users.model.js
@@ -59,6 +59,7 @@ async function getAllAccountsForUser(userId) {
                 allAccounts.push({
                     currencyName: account.currencyName,
                     network: account.network,
+                    address: account.address,
                     balance: await getBalance(account.network, account.currencyName, account.address),
                 });
             }
