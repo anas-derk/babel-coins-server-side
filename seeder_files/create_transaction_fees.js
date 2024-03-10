a7caaa96999a100ddebf9da5bd6e0e9e59a7e1df
anas derk <anas.derk2020@gmail.com>
2024-03-10 09:32:23 -0700

    Creating Transaction Fees Seeder File

---
+++ seeder_files/create_transaction_fees.js
@@ -0,0 +1,113 @@
+require("dotenv").config({ path: "../.env" });
+
+const mongoose = require("mongoose");
+
+// Create Transfer Fees Schema
+
+const transferFeesShema = mongoose.Schema({
+    depositType: {
+        type: String,
+        required: true,
+    },
+    network: String,
+    currencyName: {
+        type: String,
+        required: true,
+    },
+    symbol: {
+        type: String,
+        required: true,
+    },
+    fee: {
+        type: Number,
+        required: true,
+    },
+});
+
+// Create Fees Model From Fees Schema
+
+const transferFeesModel = mongoose.model("transfer_fees", transferFeesShema);
+
+const transfer_fees_list = [
+    {
+        depositType: "fiat",
+        currencyName: "United States dollar",
+        symbol: "USD",
+        fee: 10,
+    },
+    {
+        depositType: "fiat",
+        currencyName: "Euro",
+        symbol: "EUR",
+        fee: 10,
+    },
+    {
+        depositType: "crypto",
+        network: "TRON",
+        currencyName: "TRX",
+        symbol: "TRX",
+        fee: 0.8,
+    },
+    {
+        depositType: "crypto",
+        network: "TRON",
+        currencyName: "USDT",
+        symbol: "USDT",
+        fee: 10,
+    },
+    {
+        depositType: "crypto",
+        network: "ETHEREUM",
+        currencyName: "ETHER",
+        symbol: "ETH",
+        fee: 0.0029,
+    },
+    {
+        depositType: "crypto",
+        network: "ETHEREUM",
+        currencyName: "USDT",
+        symbol: "USDT",
+        fee: 10,
+    },
+    {
+        depositType: "crypto",
+        network: "POLYGON",
+        currencyName: "MATIC",
+        symbol: "MATIC",
+        fee: 0.1,
+    },
+    {
+        depositType: "crypto",
+        network: "POLYGON",
+        currencyName: "USDT",
+        symbol: "USDT",
+        fee: 10,
+    },
+    {
+        depositType: "crypto",
+        network: "BSC",
+        currencyName: "BNB",
+        symbol: "BNB",
+        fee: 0.0005,
+    },
+    {
+        depositType: "crypto",
+        network: "BSC",
+        currencyName: "USDT",
+        symbol: "USDT",
+        fee: 10,
+    },
+];
+
+async function create_transfer_fees_list() {
+    try {
+        await mongoose.connect(process.env.DB_URL);
+        await transferFeesModel.insertMany(transfer_fees_list);
+        return "Creating Transfer Fees List Process Has Been Successfully !!";
+    } catch(err) {
+        await mongoose.disconnect();
+        throw Error(err);
+    }
+}
+
+create_transfer_fees_list().then((result) => console.log(result));
